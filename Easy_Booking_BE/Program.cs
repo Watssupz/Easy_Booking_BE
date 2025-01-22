815b8ddd39d9d726c1311ebb38c9d98ca8e42fd4
Duong Nguyen <91860852+Watssupz@users.noreply.github.com>
2025-01-22 21:47:27 +0700

    Config API, automapper

--- Easy_Booking_BE/Program.cs
+++ Easy_Booking_BE/Program.cs
@@ -1,11 +1,23 @@
+using AutoMapper;
+using Easy_Booking_BE.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container.`
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+builder.Services.AddDbContext<EasyBookingBEContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyContr"));
+});
+
+builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
