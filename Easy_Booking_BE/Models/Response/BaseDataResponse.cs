c260d11889c545947a81e6d1b4383bfe0d99a707
Duong Nguyen <91860852+Watssupz@users.noreply.github.com>
2025-02-10 23:45:46 +0700

    Response request data

---
+++ Easy_Booking_BE/Models/Response/BaseDataResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace Easy_Booking_BE.Models.Response;
+
+public class BaseDataResponse<T>
+{
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+    public T Data { get; set; }
+
+    public BaseDataResponse(int statusCode, string message, T data)
+    {
+        StatusCode = statusCode;
+        Message = message;
+        Data = data;
+    }
+
+    public BaseDataResponse(int statusCode, string message)
+    {
+        StatusCode = statusCode;
+        Message = message;
+    }
+    
+}
