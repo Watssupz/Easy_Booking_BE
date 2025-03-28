7d271b6202a8354b1913ae831267b8878bfec8a7
Duong Nguyen <91860852+Watssupz@users.noreply.github.com>
2025-03-28 21:47:05 +0700

    response bed baths number, thumbnail

--- Easy_Booking_BE/Models/Request/RoomModel.cs
+++ Easy_Booking_BE/Models/Request/RoomModel.cs
@@ -10,9 +10,12 @@ public class RoomModel
     [Required]
     [Range(0, double.MaxValue, ErrorMessage = "Price must be greater than or equal to 0.")]
     public double price_per_night { get; set; }
-    public string? thumbnail { get; set; }
     [Required] public string description { get; set; }
     [Required] public int room_status_id { get; set; }
     [Required] public string location { get; set; }
+    public int num_beds { get; set; }
+
+    public int num_bathrooms { get; set; }
     public String? user_id { get; set; }
+    public string? thumbnail { get; set; }
 }
